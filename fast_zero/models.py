2bccdb338c73bdafbe6527baaf153f61a0bdafaa
Erick Ricioli <erick.alen@live.com>
2024-05-21 23:39:48 -0300

    new: added updated_at db column

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -17,3 +17,7 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
             init=False, server_default=func.now()
         )
+    updated_at: Mapped[datetime] = mapped_column(
+            server_default=func.now(),
+            onupdate=func.now()
+        )
